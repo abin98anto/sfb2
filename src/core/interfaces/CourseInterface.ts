3ec2408e1854429498abd2b128450d4c7c2e2e5f
abin98anto <abin98anto@gmail.com>
2025-02-17 17:19:39 +0530

    -Course repository functions created.

---
+++ src/core/interfaces/CourseInterface.ts
@@ -0,0 +1,9 @@
+import { ICourse } from "../entities/ICourse";
+
+export interface CourseInterface {
+  add(course: Partial<ICourse>): Promise<ICourse>;
+  getById(_id: string): Promise<ICourse | null>;
+  update(updates: Partial<ICourse>): Promise<ICourse | null>;
+  toggleStatus(courseId: string): Promise<boolean>;
+  all(): Promise<ICourse[]>;
+}
---
+++ src/infrastructure/repositories/CourseRepository.ts
@@ -0,0 +1,57 @@
+import { ICourse } from "../../core/entities/ICourse";
+import { CourseInterface } from "../../core/interfaces/CourseInterface";
+import { Course } from "../db/schemas/courseSchema";
+
+export class CourseRepository implements CourseInterface {
+  add = async (course: Partial<ICourse>): Promise<ICourse> => {
+    const newCourse = new Course(course);
+    await newCourse.save();
+    return newCourse;
+  };
+
+  getById = async (_id: string): Promise<ICourse | null> => {
+    return await Course.findById(_id);
+  };
+
+  update = async (updates: Partial<ICourse>): Promise<ICourse | null> => {
+    const existingCourse = await Course.findById(updates._id).exec();
+    if (!existingCourse) {
+      throw new Error("Course not found.");
+    }
+
+    for (const [key, value] of Object.entries(updates)) {
+      if (value !== undefined && key !== "_id") {
+        if (key === "basicInfo" || key === "advanceInfo") {
+          for (const [nestedKey, nestedValue] of Object.entries(
+            value as Record<string, unknown>
+          )) {
+            if (nestedValue !== undefined) {
+              (existingCourse[key] as any)[nestedKey] = nestedValue;
+            }
+          }
+        } else if (key === "curriculum") {
+          existingCourse.curriculum = value as ICourse["curriculum"];
+        } else {
+          (existingCourse as any)[key] = value;
+        }
+      }
+    }
+
+    return await existingCourse.save();
+  };
+
+  toggleStatus = async (courseId: string): Promise<boolean> => {
+    const course = await Course.findById(courseId);
+    if (!course) {
+      throw new Error("Course not found");
+    }
+
+    course.isActive = !course.isActive;
+    await course.save();
+    return true;
+  };
+
+  all = async (): Promise<ICourse[]> => {
+    return await Course.find();
+  };
+}
