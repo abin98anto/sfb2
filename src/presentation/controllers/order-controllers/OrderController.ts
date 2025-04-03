247db0f1bba07a24117fb3d9b1041e6d33e3948f
abin98anto <abin98anto@gmail.com>
2025-04-03 13:06:53 +0530

    -removed req.uesr

--- src/presentation/controllers/order-controllers/OrderController.ts
+++ src/presentation/controllers/order-controllers/OrderController.ts
@@ -70,7 +70,7 @@ class OrderController {
 
   subscriptionCheck = async (req: Request, res: Response) => {
     try {
-      const { email } = req?.user || req.params.email;
+      const { email } = req.params;
       console.log("subcheck", email);
       const result = await this.checkSubscriptionStatusUseCase.execute(email);
       res.status(200).json(result);
