ccfc67ac214772b4d46e6b5a7b11b63253629595
abin98anto <abin98anto@gmail.com>
2025-04-03 10:16:53 +0530

    -made the real url instead of * in cors.

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,7 +2,7 @@ import { API } from "../shared/constants/API";
 import { comments } from "../shared/constants/comments";
 
 export const corsOptions = {
-  origin: "*",
+  origin: ["https://sff2-second.vercel.app"],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
