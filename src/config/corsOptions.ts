1b6a7f50b466775e19de1e531b319ae71b868506
abin98anto <abin98anto@gmail.com>
2025-04-03 10:07:24 +0530

    -made changes to cors policy again.

--- src/config/corsOptions.ts
+++ src/config/corsOptions.ts
@@ -2,19 +2,7 @@ import { API } from "../shared/constants/API";
 import { comments } from "../shared/constants/comments";
 
 export const corsOptions = {
-  origin: function (
-    origin: string | undefined,
-    callback: (err: Error | null, allow?: boolean) => void
-  ) {
-    if (!origin) return callback(null, true);
-
-    const allowedOrigins = "*";
-    if (allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error(comments.CORS_FAIL));
-    }
-  },
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "Cookie"],
